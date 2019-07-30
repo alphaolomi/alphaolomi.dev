b1cc342f95b92e58e69f72cff05882642336f122
Alpha Olomi <alphaolomi@gmail.com>
2019-07-30 20:02:28 +0300

    added resume

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,9 @@ const cleanCSS = require('gulp-clean-css');
 const concatCss = require('gulp-concat-css');
 const image = require('gulp-image');
 const gulpSeo = require('gulp-seo');
-const gulpFont= require('gulp-font');
 const robots = require('gulp-robots');
 const sitemap = require('gulp-sitemap');
 const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
 const del = require('delete');
 
 /**
@@ -25,14 +23,14 @@ const del = require('delete');
  */
 
 function defaultTask(cb) {
- 
-   return src('src/index.html', {
+
+    return src('src/index.html', {
         read: false
     })
-    .pipe(sitemap({
-        siteUrl: 'https://alphaolomi.com'
-    }))
-    .pipe(dest('dist/'));
+        .pipe(sitemap({
+            siteUrl: 'https://alphaolomi.com'
+        }))
+        .pipe(dest('dist/'));
 
 }
 
@@ -40,29 +38,29 @@ exports.default = defaultTask
 
 function html(cb) {
 
-    return src('src/index.html')
-    // src('views/index.html')
-    .pipe(gulpSeo({
-        list: ['og', 'se', 'schema', 'twitter'],
-        meta: {
-            title: 'Alpha Olomi // Software Developer',
-            description: 'Alpha Olomi is a Software Developer with over 3 years of experiences in a wide range of disciplines. Constantly in back-end development, learning and experiencing new things.',
-            author: 'Alpha olomi',
-            keywords: ['Alpha Olomi','Alpha Elifuraha Olomi','personal','portfolio','experience','freelance','student','engineer','software','developer'],
-            robots: {
-                index: true, // true
-                follow: true // true
-            },
-            revisitAfter: '5 month', // 3 month
-            contact :'hello@alphaolomi.com',
-            image: 'http://alphaolomi.com/img/thumbnail.png',
-            site_name: 'Alpha Olomi // Software Developer',
-            type: 'profile'
- 
-        }
-    }))
-    .pipe(minifyHTML())
-    .pipe(dest('dist'))
+    return src('src/*.html')
+        // src('views/index.html')
+        .pipe(gulpSeo({
+            list: ['og', 'se', 'schema', 'twitter'],
+            meta: {
+                title: 'Alpha Olomi // Software Developer',
+                description: 'Alpha Olomi is a Software Developer with over 3 years of experiences in a wide range of disciplines. Constantly in back-end development, learning and experiencing new things.',
+                author: 'Alpha olomi',
+                keywords: ['Alpha Olomi', 'Alpha Elifuraha Olomi', 'personal', 'portfolio', 'experience', 'freelance', 'student', 'engineer', 'software', 'developer'],
+                robots: {
+                    index: true, // true
+                    follow: true // true
+                },
+                revisitAfter: '5 month', // 5 month
+                contact: 'hello@alphaolomi.com',
+                image: 'http://alphaolomi.com/img/thumbnail.png',
+                site_name: 'Alpha Olomi // Software Developer',
+                type: 'profile'
+
+            }
+        }))
+        .pipe(minifyHTML())
+        .pipe(dest('dist'))
 }
 
 function javascript(cb) {
@@ -82,30 +80,30 @@ function javascript(cb) {
 function css(cb) {
 
     return src('src/css/*.css')
-    .pipe(cleanCSS())
-    .pipe(concatCss("main.min.css"))
-    .pipe(dest('dist/css/'))
+        .pipe(cleanCSS())
+        .pipe(concatCss("main.min.css"))
+        .pipe(dest('dist/css/'))
 
 
 }
 
 function imageMin(cb) {
     return src('src/img/*.+(png|jpg|gif)')
-    .pipe(image())
-    .pipe(dest('dist/img'));
+        .pipe(image())
+        .pipe(dest('dist/img'));
 }
 
 function favicon(cb) {
     return src('src/favicon/*')
-    // .pipe(image())
-    .pipe(dest('dist/favicon'));
+        // .pipe(image())
+        .pipe(dest('dist/favicon'));
 }
 
 function font(cb) {
-  
+
     return src('src/fonts/*.{eot,ttf,woff}')
 
-    .pipe(dest('dist/fonts'));
+        .pipe(dest('dist/fonts'));
 
 }
 
@@ -116,7 +114,7 @@ function robot(cb) {
             allow: ['/'],
             disallow: ['cgi-bin/']
         }))
-        .pipe(dest('dist/robots.txt'));
+        .pipe(dest('dist/'));
 }
 
 
@@ -124,14 +122,13 @@ function site_map(cb) {
     return src('src/index.html', {
         read: false
     })
-    .pipe(sitemap({
-        siteUrl: 'https://alphaolomi.com'
-    }))
-    .pipe(dest('dist/'));
+        .pipe(sitemap({
+            siteUrl: 'https://alphaolomi.com'
+        }))
+        .pipe(dest('dist/'));
 }
 
-exports.build = parallel(html, javascript, css,imageMin, font,favicon, robot,site_map);
-exports.favicon = favicon;
+exports.build = parallel(html, javascript, css, imageMin, font, favicon, robot, site_map);
 
 function clean(cb) {
     // Use the `delete` module directly, instead of using gulp-rimraf
