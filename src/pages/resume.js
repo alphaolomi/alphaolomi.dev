91eb12346b63d257dfe497fb0e43beb63641de79
Alpha Olomi <alphaolomi@gmail.com>
2019-09-23 18:21:04 +0300

    migration

---
+++ src/pages/resume.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ResumePage = () => (
+  <Layout>
+    <SEO title="Resume" />
+    <h1>Hi from the second page</h1>
+    <p>Welcome to page 2</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default ResumePage
