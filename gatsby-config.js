0ef45880eae495384ad880244ecb262abef15d0d
Alpha Olomi <alphaolomi@gmail.com>
2021-09-06 14:52:57 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Alpha Olomi // Software Engineer`,
+        short_name: `Alpha Olomi`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
