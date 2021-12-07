bcbefd2f822c5e47ab35b90f816bdbc8e1e0f3ad
Alpha Olomi <alphaolomi@gmail.com>
2021-12-07 03:00:41 +0300

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Hero from './Hero/Hero';
+import About from './About/About';
 
 import { PortfolioProvider } from '../context/context';
 
@@ -7,14 +8,14 @@ import { heroData } from '../mock/data';
 
 function App() {
   const [hero, setHero] = useState({});
-  // const [about, setAbout] = useState({});
+  const [about, setAbout] = useState({});
   // const [projects, setProjects] = useState([]);
   // const [contact, setContact] = useState({});
   // const [footer, setFooter] = useState({});
 
   useEffect(() => {
     setHero({ ...heroData });
-    // setAbout({ .../aboutData });
+    setAbout({ ...aboutData });
     // setProjects([...projectsData]);
     // setContact({ ...contactData });
     // setFooter({ ...footerData });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,18 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import Head from 'next/head';
 import App from '../components/App';
-import { siteMetadata, headData } from '../mock/data';
+import { headData } from '../mock/data';
 
 const IndexPage = () => {
   const { title, lang, description } = headData;
   return (
     <>
-      <Helmet
-        meta={[
-          {
-            name: `description`,
-            content: siteMetadata.description,
-          },
-          {
-            name: `google-site-verification`,
-            content: '',
-          },
-          {
-            property: `og:title`,
-            content: siteMetadata.title,
-          },
-          {
-            property: `og:description`,
-            content: siteMetadata.description,
-          },
-          {
-            property: `og:type`,
-            content: `website`,
-          },
-          {
-            name: `twitter:card`,
-            content: `summary`,
-          },
-          {
-            name: `twitter:creator`,
-            content: siteMetadata.author,
-          },
-          {
-            name: `twitter:title`,
-            content: siteMetadata.title,
-          },
-          {
-            name: `twitter:description`,
-            content: siteMetadata.description,
-          },
-        ]}
-      >
+      <Head>
         <meta charSet="utf-8" />
         <title>{title || 'Gatsby Simplefolio'}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
-      </Helmet>
+      </Head>
       <App />
     </>
   );
