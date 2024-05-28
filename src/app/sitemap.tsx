d3e129a3951856847ad860bb9027360f1129de37
Alpha Olomi <alphaolomi@gmail.com>
2024-05-28 23:23:25 +0300

    Update sitemap.tsx

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,4 +1,4 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
@@ -7,6 +7,78 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
-    }
-  ]
+    },
+    // {
+    //   url: 'https://alphaolomi.dev/_not-found',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'yearly',
+    //   priority: 0.5,
+    // },
+    {
+      url: 'https://alphaolomi.dev/about',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://alphaolomi.dev/articles',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://alphaolomi.dev/articles/upgrade-to-next14',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://alphaolomi.dev/articles/website-migration',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://alphaolomi.dev/feed.xml',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://alphaolomi.dev/guestbook',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://alphaolomi.dev/opensource',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    // {
+    //   url: 'https://alphaolomi.dev/sitemap.xml',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'weekly',
+    //   priority: 0.6,
+    // },
+    {
+      url: 'https://alphaolomi.dev/speaking',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    // {
+    //   url: 'https://alphaolomi.dev/thank-you',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'yearly',
+    //   priority: 0.5,
+    // },
+    {
+      url: 'https://alphaolomi.dev/uses',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
 }
