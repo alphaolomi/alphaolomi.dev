9a2c98f5f1ac9d41e437b5dfe2725f55dd261170
Alpha Olomi <alphaolomi@gmail.com>
2024-05-28 22:06:45 +0300

    Replace Google Analytics script with @next/third-parties package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { type Metadata } from 'next'
-import Script from 'next/script'
-
+import { GoogleAnalytics } from '@next/third-parties'
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
@@ -35,20 +34,7 @@ export default function RootLayout({
             <Layout>{children}</Layout>
           </div>
         </Providers>
-
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
-        </Script>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
       </body>
     </html>
   )
